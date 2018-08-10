dc950d209d78da6b72ac26599e95d11e7ef6cf96
stvansolano <stvansolano@outlook.com>
2018-08-09 21:39:09 -0600

    Hello. World!

---
+++ Backend/Backend/Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+	[Route("api/")]
+	public class ApiController : Controller
+	{
+		[HttpGet]
+		[Route("hello")]
+		public string Get()
+		{
+			return "Hello, World!";
+		}
+
+		[HttpGet]
+		[Route("hello/{name}")]
+		public string Get(string name)
+		{
+			return "Hello, " + name;
+		}
+	}
+}
