499ef9918caa07b97eb8434cf9c927e7602bbe39
stvansolano <stvansolano@outlook.com>
2018-08-09 22:22:34 -0600

    UnitTests

---
+++ Backend/UnitTests/BackendTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Backend.Controllers;
+using Xunit;
+
+namespace UnitTests
+{
+	public class BackendTests
+	{
+		[Fact]
+		public void SayHello()
+		{
+			// Arrange
+			var controller = new ApiController();
+
+			// Act
+			var result = controller.Get();
+
+			// Assert
+			Assert.Equal("Hello, World!", result);
+		}
+	}
+}
